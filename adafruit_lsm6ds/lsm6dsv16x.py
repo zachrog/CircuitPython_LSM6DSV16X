c61660c572a6e0879b12f12b219e53a35b1f1669
Cian Rodriguez <crod0007@student.monash.edu>
2024-09-05 09:46:00 +1000

    begin implementation of v16x

---
+++ adafruit_lsm6ds/lsm6dsv16x.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020 Bryan Siepert for Adafruit Industries
+# Modified by Cian Rodriguez for Dingo V2
+# SPDX-License-Identifier: MIT
+"""
+This module provides the `adafruit_lsm6ds.LSM6DSV16X` subclass of LSM6DS sensors
+==============================================================================
+"""
+from . import LSM6DS, LSM6DS_DEFAULT_ADDRESS, LSM6DS_CHIP_ID
+
+try:
+    import typing  # pylint: disable=unused-import
+    from busio import I2C
+except ImportError:
+    pass
+
+
+class LSM6DSV16X(LSM6DS):  # pylint: disable=too-many-instance-attributes
+
+    """Driver for the LSM6DSV16X 6-axis accelerometer and gyroscope.
+
+    :param ~busio.I2C i2c_bus: The I2C bus the LSM6DSV16X is connected to.
+    :param int address: The I2C device address. Defaults to :const:`0x6A`
+
+
+    **Quickstart: Importing and using the device**
+
+        Here is an example of using the :class:`LSM6DSV16X` class.
+        First you will need to import the libraries to use the sensor
+
+        .. code-block:: python
+
+            import board
+            from adafruit_lsm6ds.LSM6DSV16X import LSM6DSV16X
+
+        Once this is done you can define your `board.I2C` object and define your sensor object
+
+        .. code-block:: python
+
+            i2c = board.I2C()  # uses board.SCL and board.SDA
+            sensor = LSM6DSV16X(i2c)
+
+        Now you have access to the :attr:`acceleration` and :attr:`gyro`: attributes
+
+        .. code-block:: python
+
+            acc_x, acc_y, acc_z = sensor.acceleration
+            gyro_x, gyro_z, gyro_z = sensor.gyro
+
+    """
+
+    # CHIP_ID = LSM6DS_CHIP_ID
+    CHIP_ID = 0x70
+
+    def __init__(
+        self, i2c_bus: I2C, address: int = LSM6DS_DEFAULT_ADDRESS, ucf: str = None
+    ) -> None:
+        super().__init__(i2c_bus, address, ucf)
+        self._i3c_disable = True
